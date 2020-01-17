1bb780a3dcc95b5493e6764da6b3b9b4fc286483
Yixi <lyh1112@gmail.com>
2020-01-17 14:58:52 +0800

    add more state icon for jobs

--- src/Pipline/Jobs.tsx
+++ src/Pipline/Jobs.tsx
@@ -8,6 +8,8 @@ const Job: React.FC<{ job: any }> = ({job}) => {
         {jobData.label}
         {jobData.passed && <i className="icon check" />}
         {jobData.passed === false && jobData.state === 'FINISHED' && <i className="icon error"/>}
+        {jobData.state === 'RUNNING' && <i className="icon loading"/>}
+        {jobData.state === 'WAITING' && <i className="icon waiting"/>}
       </div>
     </div>
   )

f90f99a297964761b25507b35cc31d0a79c774e8
guzhongren <guzhongren@live.cn>
2020-09-27 21:54:26 +0800

    fix(service work): fix service work issue

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,13 @@ import * as ReactDOM from 'react-dom'
 import App from '@root/App'
 
 ReactDOM.render(<App/>, document.getElementById('app'))
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration)
+        }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+        })
+    })
+}

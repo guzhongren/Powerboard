a7653b24c8c7e46a9f6d829c73a21b6a85ed62f9
guzhongren <guzhongren@live.cn>
2020-09-27 22:34:46 +0800

    fix(servicework): fix service work bug

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ ReactDOM.render(<App/>, document.getElementById('app'))
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        navigator.serviceWorker.register('./service-worker.js').then(registration => {
         console.log('SW registered: ', registration)
         }).catch(registrationError => {
         console.log('SW registration failed: ', registrationError)

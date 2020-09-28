74c120d8ba78641c2e8d92b72452df4675358c4a
guzhongren <guzhongren@live.cn>
2020-09-28 16:37:29 +0800

    test(servicework): test service worker

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./service-worker.js').then(registration => {
         console.log('SW registered: ', registration)
         }).catch(registrationError => {
-        console.log('SW registration failed: ', registrationError)
+        console.log('SW registration failed:  ', registrationError)
         })
     })
 }

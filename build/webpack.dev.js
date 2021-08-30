62a0a56e3acd8da0a8a8ff3da6643bddafd9a711
guzhongren <guzhongren@live.cn>
2021-08-30 13:31:32 +0800

    feat(cypress): add intergration test

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -22,21 +22,12 @@ module.exports = {
   context: path.resolve(__dirname, "../src"),
   devtool: "cheap-module-source-map",
   devServer: {
-    stats: {
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false,
-    },
     hot: true,
     // enable HMR on the server
     compress: true,
-    contentBase: path.resolve(__dirname, '../src'),
     // match the output path
     port: PORT,
     host: _HOST,
-    publicPath: URL,
     historyApiFallback: true,
   },
   mode: "development",

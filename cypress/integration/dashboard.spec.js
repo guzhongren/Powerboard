b10132e7ef53390b485de60745483176dd251c5c
guzhongren <guzhongren@live.cn>
2022-03-18 23:11:09 +0800

    chore(e2e): chore e2e structure

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -4,29 +4,25 @@ import { convertToString } from '../../src/Utils/ConvertUtils'
 import {
   ID_PIPELINES,
   ID_IMPORT,
-  LABEL_INPUT,
-  LABEL_PIPELINE_AREA,
   CLASS_BUTTON_GO,
   TITLE_ORG_NAME,
   TITLE_ACCESS_TOKEN,
-  TITLE_PIPELINE,
   TITLE_PIPELINE_REPO,
   ID_ONCALL_LIST,
 } from '../fixtures/fixtures'
+
+import {
+  checkPipelineTitle,
+  checkPipelineRepo,
+  checkAuthInput,
+  checkLocalStorageInfo,
+  checkAuthInfo,
+} from '../page/check'
 
 describe('show pipeline', () => {
   dashboardConfig.token = Cypress.env('BUILDKITE_TOKEN')
   const orgName = dashboardConfig.orgName
   const pipelinesConfig = dashboardConfig.pipelines
-
-  const DASHBOARD_AUTH = {
-    ORG: 'org',
-    TEAM: 'team',
-    SEARCH: 'search',
-    TOKEN: 'token',
-    ONCALL: 'oncall',
-    IS_ONLY_MAIN_BRANCH: 'isOnlyMainBranch',
-  }
 
   describe('display pipeline by manuel config', () => {
     beforeEach(() => {
@@ -53,14 +49,14 @@ describe('show pipeline', () => {
 
       const newConfig = {
         ...dashboardConfig,
-        isOnlyMainBranch: '',
+        isOnlyMainBranch: false,
         pipelines: [dashboardConfig.pipelines[0]],
       }
       cy.get(CLASS_BUTTON_GO)
         .click()
         .then(() => {
           checkLocalStorageInfo(newConfig)
-          checkPipelineTitle(1)
+          checkPipelineTitle(1, pipelinesConfig[0])
         })
       // should update the pipeline settings
 
@@ -77,7 +73,7 @@ describe('show pipeline', () => {
           })
         })
         .then(() => {
-          checkPipelineTitle(2)
+          checkPipelineTitle(2, pipelinesConfig[0])
           checkPipelineRepo(TITLE_PIPELINE_REPO)
         })
     })
@@ -137,7 +133,7 @@ describe('show pipeline', () => {
         .click()
         .then(() => {
           checkLocalStorageInfo(newConfig)
-          checkPipelineTitle(2)
+          checkPipelineTitle(2, pipelinesConfig[0])
         })
     })
     it('should download the latest auth config when click download button', () => {
@@ -196,77 +192,8 @@ describe('show pipeline', () => {
           dashboardConfig.configPath
         }`
       )
-      checkPipelineTitle(2)
+      checkPipelineTitle(2, pipelinesConfig[0])
       checkPipelineRepo(TITLE_PIPELINE_REPO)
     })
   })
-
-  function checkPipelineTitle(countOfPipelines) {
-    cy.get(TITLE_PIPELINE).should('have.length', countOfPipelines)
-    cy.get(TITLE_PIPELINE).first().should('have.attr', 'href')
-    cy.get(TITLE_PIPELINE).first().should('have.attr', 'target', '_blank')
-    cy.get(TITLE_PIPELINE).first().should('have.text', pipelinesConfig[0])
-  }
-
-  function checkPipelineRepo(repoCssSelector) {
-    cy.get(repoCssSelector).should('have.length', 2)
-    cy.get(repoCssSelector)
-      .first()
-      .parent()
-      .should('have.attr', 'target', '_blank')
-  }
-
-  function checkAuthInput() {
-    cy.get(LABEL_INPUT).should('have.length', 5)
-
-    cy.get(LABEL_INPUT).first().should('have.text', '')
-    cy.get(LABEL_INPUT).last().should('have.text', '')
-    cy.get(ID_PIPELINES).should('have.text', '')
-    cy.get('input[type="checkbox"]').should('not.be.checked')
-
-    cy.get(LABEL_PIPELINE_AREA).should('have.length', 2)
-  }
-
-  function checkLocalStorageInfo(dashboardConfig) {
-    expect(dashboardConfig.orgName).to.equal(
-      localStorage.getItem(DASHBOARD_AUTH.ORG)
-    )
-    expect(dashboardConfig.team).to.equal(
-      localStorage.getItem(DASHBOARD_AUTH.TEAM)
-    )
-    expect(convertToString(dashboardConfig.pipelines)).to.equal(
-      localStorage.getItem(DASHBOARD_AUTH.SEARCH)
-    )
-    expect(dashboardConfig.token).to.equal(
-      localStorage.getItem(DASHBOARD_AUTH.TOKEN)
-    )
-    expect(convertToString(dashboardConfig.oncall)).to.equal(
-      localStorage.getItem(DASHBOARD_AUTH.ONCALL)
-    )
-    expect(`${dashboardConfig.isOnlyMainBranch}`).to.equal(
-      localStorage.getItem(DASHBOARD_AUTH.IS_ONLY_MAIN_BRANCH)
-    )
-  }
-
-  function checkAuthInfo(dashboardConfig) {
-    cy.contains(TITLE_ACCESS_TOKEN)
-      .parent()
-      .find('input[type=password]')
-      .should('have.value', dashboardConfig.token)
-    cy.contains(TITLE_ORG_NAME)
-      .parent()
-      .find('input[type=text]')
-      .should('have.value', orgName)
-    cy.get(ID_PIPELINES).should(
-      'have.value',
-      dashboardConfig.pipelines.join('\n')
-    )
-    cy.get(ID_ONCALL_LIST).should(
-      'have.value',
-      convertToString(dashboardConfig.oncall)
-    )
-    cy.get('input[type="checkbox"]').should(
-      `${dashboardConfig.isOnlyMainBranch ? '' : 'not.'}be.checked`
-    )
-  }
 })
---
+++ cypress/page/check.js
@@ -0,0 +1,89 @@
+import {
+  ID_PIPELINES,
+  LABEL_INPUT,
+  LABEL_PIPELINE_AREA,
+  TITLE_ORG_NAME,
+  TITLE_ACCESS_TOKEN,
+  TITLE_PIPELINE,
+  ID_ONCALL_LIST,
+} from '../fixtures/fixtures'
+
+import { convertToString } from '../../src/Utils/ConvertUtils.ts'
+
+const DASHBOARD_AUTH = {
+  ORG: 'org',
+  TEAM: 'team',
+  SEARCH: 'search',
+  TOKEN: 'token',
+  ONCALL: 'oncall',
+  IS_ONLY_MAIN_BRANCH: 'isOnlyMainBranch',
+}
+
+export function checkPipelineTitle(countOfPipelines, firstPipelineName) {
+  cy.get(TITLE_PIPELINE).should('have.length', countOfPipelines)
+  cy.get(TITLE_PIPELINE).first().should('have.attr', 'href')
+  cy.get(TITLE_PIPELINE).first().should('have.attr', 'target', '_blank')
+  cy.get(TITLE_PIPELINE).first().should('have.text', firstPipelineName)
+}
+
+export function checkPipelineRepo(repoCssSelector) {
+  cy.get(repoCssSelector).should('have.length', 2)
+  cy.get(repoCssSelector)
+    .first()
+    .parent()
+    .should('have.attr', 'target', '_blank')
+}
+
+export function checkAuthInput() {
+  cy.get(LABEL_INPUT).should('have.length', 5)
+
+  cy.get(LABEL_INPUT).first().should('have.text', '')
+  cy.get(LABEL_INPUT).last().should('have.text', '')
+  cy.get(ID_PIPELINES).should('have.text', '')
+  cy.get('input[type="checkbox"]').should('not.be.checked')
+
+  cy.get(LABEL_PIPELINE_AREA).should('have.length', 2)
+}
+
+export function checkLocalStorageInfo(dashboardConfig) {
+  expect(dashboardConfig.orgName).to.equal(
+    localStorage.getItem(DASHBOARD_AUTH.ORG)
+  )
+  expect(dashboardConfig.team).to.equal(
+    localStorage.getItem(DASHBOARD_AUTH.TEAM)
+  )
+  expect(convertToString(dashboardConfig.pipelines)).to.equal(
+    localStorage.getItem(DASHBOARD_AUTH.SEARCH)
+  )
+  expect(dashboardConfig.token).to.equal(
+    localStorage.getItem(DASHBOARD_AUTH.TOKEN)
+  )
+  expect(convertToString(dashboardConfig.oncall)).to.equal(
+    localStorage.getItem(DASHBOARD_AUTH.ONCALL)
+  )
+  expect(`${dashboardConfig.isOnlyMainBranch}`).to.equal(
+    localStorage.getItem(DASHBOARD_AUTH.IS_ONLY_MAIN_BRANCH)
+  )
+}
+
+export function checkAuthInfo(dashboardConfig) {
+  cy.contains(TITLE_ACCESS_TOKEN)
+    .parent()
+    .find('input[type=password]')
+    .should('have.value', dashboardConfig.token)
+  cy.contains(TITLE_ORG_NAME)
+    .parent()
+    .find('input[type=text]')
+    .should('have.value', dashboardConfig.orgName)
+  cy.get(ID_PIPELINES).should(
+    'have.value',
+    dashboardConfig.pipelines.join('\n')
+  )
+  cy.get(ID_ONCALL_LIST).should(
+    'have.value',
+    convertToString(dashboardConfig.oncall)
+  )
+  cy.get('input[type="checkbox"]').should(
+    `${dashboardConfig.isOnlyMainBranch ? '' : 'not.'}be.checked`
+  )
+}

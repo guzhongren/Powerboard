efa5c1f93e7a304073c07f0f0119433d87ed84ee
guzhongren <guzhongren@live.cn>
2022-03-19 21:43:20 +0800

    revert(webpack): Revert 4d0ca89077ac327857d5368fff4b9a17a0c5be57

--- build/webpack.build.js
+++ build/webpack.build.js
@@ -1,15 +1,33 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { GenerateSW } = require('workbox-webpack-plugin')
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common')
+const CopyPlugin = require('copy-webpack-plugin')
 
-module.exports = merge(common, {
+module.exports = {
   entry: '../src/index.tsx',
+  output: {
+    filename: '[name]-[fullhash:7].js',
+    path: path.resolve(__dirname, '../dist'),
+    chunkFilename: '[chunkhash].js',
+    publicPath: '',
+  },
+  context: path.resolve(__dirname, '../src'),
   bail: true,
   mode: 'production',
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
       {
         test: /\.scss$/i,
         use: [
@@ -33,7 +51,21 @@ module.exports = merge(common, {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(jpe?g|png|gif|ogg|mp3)$/,
+        use: ['url-loader'],
+      },
+      {
+        test: /\.(svg?)(\?[a-z0-9]+)?$/,
+        use: ['url-loader'],
+      },
     ],
+  },
+  resolve: {
+    alias: {
+      '@root': path.resolve(__dirname, '../src'),
+    },
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
   stats: {
     cached: true,
@@ -43,13 +75,30 @@ module.exports = merge(common, {
     modules: false,
   },
   plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        enabled: true,
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+        tslint: path.resolve(__dirname, '../tslint.json'),
+      },
+    }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].[fullhash:7].css',
       chunkFilename: '[id].[fullhash:7].css',
     }),
+    new HTMLWebpackPlugin({
+      template: '../public/index.html',
+      filename: 'index.html',
+    }),
     new GenerateSW({
       skipWaiting: true,
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: '../public/manifest.json', to: 'manifest.json' },
+        { from: '../public/robots.txt', to: 'robots.txt' },
+      ],
+    }),
   ],
-})
+}

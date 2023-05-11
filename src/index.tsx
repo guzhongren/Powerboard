cd44788d8ba298298c4577fcd7f93039bd8218e1
guzhongren <guzhongren@live.cn>
2023-05-11 08:02:40 +0800

    feat(pwa): add vite pwa plugin

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,3 @@ import React from 'react'
 createRoot(document.getElementById('app')).render(
   <App />,
 )
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('./service-worker.js')
-      .then((registration) => {
-        console.log('SW registered: ', registration)
-      })
-      .catch((registrationError) => {
-        console.log('SW registration failed:  ', registrationError)
-      })
-  })
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.ci == 'true' ? '/Powerboard' : '',
-  plugins: [react()],
+  base: process.env.ci === 'true' ? '/Powerboard' : '',
+  plugins: [react(), VitePWA()],
 })
